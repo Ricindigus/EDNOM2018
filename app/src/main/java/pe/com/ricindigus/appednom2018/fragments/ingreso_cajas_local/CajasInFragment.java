93ef92321b79d2de8b253dfea2e6ced425cb6fec
Ricardo Morales <ricindigus@gmail.com>
2018-08-12 23:34:58 -0500

    definicion de estructura general

---
+++ app/src/main/java/pe/com/ricindigus/appednom2018/fragments/ingreso_cajas_local/CajasInFragment.java
@@ -0,0 +1,30 @@
+package pe.com.ricindigus.appednom2018.fragments.ingreso_cajas_local;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pe.com.ricindigus.appednom2018.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CajasInFragment extends Fragment {
+
+
+    public CajasInFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cajas_in, container, false);
+    }
+
+}
---
+++ app/src/main/java/pe/com/ricindigus/appednom2018/fragments/registro_control_inventario/InvCuaderFragment.java
@@ -0,0 +1,30 @@
+package pe.com.ricindigus.appednom2018.fragments.registro_control_inventario;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pe.com.ricindigus.appednom2018.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InvCuaderFragment extends Fragment {
+
+
+    public InvCuaderFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inv_cuader, container, false);
+    }
+
+}
